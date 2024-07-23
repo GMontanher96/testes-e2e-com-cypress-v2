de97761ed7c5fb3c17cba6d96ed32ce02fc85bc6
Montanher <gabrielmontanherr@gmail.com>
2024-07-23 14:56:02 -0300

    Updates login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,12 @@
 // cypress/e2e/login.cy.js
 
-describe('Login', () => {
-    it('successfully logs in', () => {
-      cy.intercept('GET', '**/notes').as('getNotes')
+describe("Login", () => {
+  it("successfully logs in", () => {
+    cy.intercept("GET", "**/notes").as("getNotes")
 
-      cy.login()
-      cy.wait('@getNotes')
+    cy.login()
+    cy.wait("@getNotes")
 
-      cy.contains('a', 'Create a new note').should('be.visible')
-    })
+    cy.contains("a", "Create a new note").should("be.visible")
   })
-  
+})

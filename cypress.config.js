2998929396c371f95af435bb2c29b10afd40b01a
Montanher <gabrielmontanherr@gmail.com>
2024-07-24 14:54:17 -0300

    configure

--- cypress.config.js
+++ cypress.config.js
@@ -9,8 +9,6 @@ module.exports = defineConfig({
     env: {
       viewportWidthBreakpoint: 768,
     },
-    setupNodeEvents(on, _config) {
-      // implement node event listeners here
-    },
   },
+  projectId: 'cza817',
 })
